e459e730d3a748136802d7a21d6c9195c78ff2c2
Marijus Kilmanas <mkilmanas@inviqa.com>
2015-08-12 11:20:59 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # git-pr
+Git utility to allow quick GitHub Pull Request (PR) creation from command line using current active branch.
+
+## Setup
+1. Make this script executable: `chmod +x git-pr.sh`
+2. Add it to local bin: `ln -s /path/to/git-pr.sh /usr/local/bin/git-pr`
+3. Add alias into `~/.gitconfig`:
+```
+[alias]
+    pr = "!git-pr"
+```
+
+## Config (optional)
+You can specify the default branch against which the PR's should be created against in your git config.
+You can set one value in your global config:
+```
+git config --add --global pr.default-branch master
+```
+And then you can specify a different value per each git repository:
+```
+git config --add pr.default-branch develop
+```
+If you choose not to have configured default(s), you will need to specify the branch on each command run
+
+# Usage
+```
+git pr [--help|-h] [<branch>]
+
+git pr                    -- make a PR against the configured default branch
+git pr foo/bar            -- make a PR against branch foo/bar
+git pr --help             -- show brief usage help
+```
+
+# Caveat
+The remote repository to use is determined by taking the first entry from remotes list. This might be important if you work with more than one remote repository (e.g. forks)
+
+# TODO
+- Integrate with git autocomplete for branch names
+
+# Authorship
+Inspired and initial development by @asarturas
+Updates and publishing by @mkilmanas
+PRs, issues an comments welcome
---
+++ git-pr.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Installation and usage:
+# See README at https://github.com/mkilmanas/git-pr/blob/master/README.md
+
+REPOSITORY=`git remote -v | grep -oE --max-count=1 '[^:]+/[^.]+'`
+SOURCE=`git rev-parse --abbrev-ref HEAD`
+TARGET=$1
+
+if echo -e foo | grep -v '^-e' > /dev/null
+then
+    ECHO_FLAG='-e'
+fi
+
+if [ "$TARGET"="--help" -o "$TARGET"="-h" ]; then
+    echo $ECHO_FLAG "Usage: git pr [<branch>]"
+    echo $ECHO_FLAG "See https://github.com/mkilmanas/git-pr/blob/master/README.md for more details"
+    echo ""
+    exit 1
+fi
+
+if [ -z $TARGET ]; then
+    TARGET=`git config --get pr.default-branch`
+fi
+
+if [ -z $TARGET ]; then
+    echo $ECHO_FLAG "\033[0;31mNo branch specified to create the pull request against"
+    echo $ECHO_FLAG "You need to either specify it as a parameter, e.g.:"
+    echo $ECHO_FLAG "    \033[0;32mgit pr master\033[0;31m"
+    echo $ECHO_FLAG "Or you can set it as pr.default-branch in git config:"
+    echo $ECHO_FLAG "    \033[0;32mgit config --add [--global] pr.default-branch master\033[0;31m"
+    echo $ECHO_FLAG "\033[0m"
+    exit 1;
+fi
+
+URL_FORMAT="https://github.com/%s/compare/%s...%s"
+URL=$(printf $URL_FORMAT $REPOSITORY $TARGET $SOURCE)
+
+echo $ECHO_FLAG "\033[0;33m"$URL"\033[0m"
+
+if which x-www-browser > /dev/null
+then
+  x-www-browser $URL
+elif which gnome-open > /dev/null
+then
+  gnome-open $URL
+elif which xdg-open > /dev/null
+then
+  xdg-open $URL
+else
+  open $URL
+fi
